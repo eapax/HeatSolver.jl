8dd04fdeaef1fc3fcdcbe7006c1dc0227a6ab8f3
Adam Paxton <adampaxton9973@gmail.com>
2020-10-02 14:43:40 +0100

    add type-flexibility

--- heatsolver.jl
+++ heatsolver.jl
@@ -1,25 +1,29 @@
-#Set parameters
-function heatsolve(;
+
+function heatsolve(::Type{MyType};
 t_start = 0,      #start time (s)
 t_stop = 100*365*24*60*60,     #stop time (s)
 H = 60,            #soil depth (m)
 dt = 1800,          #time step (s)
 dz = 1,           #height step (m)
-D = 7 * 10^(-7))      #soil diffusivity (m^2s^-1)
+D = 7 * 10^(-7)      #soil diffusivity (m^2s^-1)
+) where MyType<:AbstractFloat
     #Define initial & boundary condition at surface (z=0)
-    N = convert(Int64, (t_stop - t_start) ÷ dt)
-    M = convert(Int64, H ÷ dz + 1)
-    IC = 273.15 * ones(M)
-    BC = 280 * ones(N)
+    N = Int64.((t_stop - t_start) ÷ dt)
+    M = Int64.(H ÷ dz + 1)
+    IC = MyType.(273.15 * ones(M))
+    BC = MyType.(280 * ones(N))
 
     #Initialize array for temperature
-    T = zeros(M, N)
+    T = MyType.(zeros(M, N))
     T[1,:] = BC
     T[:,1] = IC
 
     #Solve for T
     #using finite difference in space and forward Euler in time
     #and "perfect insulation" neuman boundary condition dT/dz(t,-H)=0 at soil bottom
+    dt = MyType.(dt)
+    dz = MyType.(dz)
+    D = MyType.(D)
     ξ = dt*D*(dz^(-2))
     for n in 1:N-1
         for m in 2:M-1
@@ -28,4 +32,12 @@ D = 7 * 10^(-7))      #soil diffusivity (m^2s^-1)
         T[M,n+1] = T[M,n] + ξ*(-T[M,n]+T[M-1,n])
     end
     return T
+end
+
+using Plots
+
+function heatplot(::Type{MyType}) where MyType <: AbstractFloat
+    T = heatsolve(MyType)
+    Tweekly = T[:,begin:336:end]
+    heatmap(Tweekly)
 end
